50a3f0685828d8e69d320f8f14fffe7e6bdb506d
Mark Bashqawi <mbashqawi@gmail.com>
2019-09-29 17:19:10 -0500

    Added viewer sizer and widget layout

--- src/AirfoilViewer.cpp
+++ src/AirfoilViewer.cpp
@@ -2,6 +2,18 @@
 
 ViewerPanel::ViewerPanel(wxWindow* parent)
 	: wxPanel(parent){
+
+	// Sizer that controls the overall layout of the airfoil viewer
+	wxBoxSizer* avTopSizer = new wxBoxSizer(wxVERTICAL);
+
+	// Drawing area for the airfoil plot
+	wxSizerItem* avDrawArea = avTopSizer->Add(new wxPanel(this,-1,wxDefaultPosition,wxSize(800,500)), wxSizerFlags().Center());
+	// List to hold loaded airfoils
+	wxListBox* airfoilListBox = new wxListBox(this, -1);
+	avTopSizer->Add(airfoilListBox, wxSizerFlags().Expand());
+
+	this->SetSizer(avTopSizer);
+
 }
 
 AirfoilViewer::AirfoilViewer(wxWindow* parent) {
