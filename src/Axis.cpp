ae46b1d0a82d5093fa1a3c47e69af133a05bf45c
Mark Bashqawi <mbashqawi@gmail.com>
2019-10-08 14:38:55 -0500

    Added implementation of draw function

--- src/Axis.cpp
+++ src/Axis.cpp
@@ -23,22 +23,20 @@ void Axis::calculateVLocs() {
 void Axis::draw(wxDC& dc) {
 	dc.SetPen(wxPen(*wxRED, 1));
 	dc.SetTextForeground(*wxRED);
-	dc.DrawLine(100, 100, 80, 90);
-	//dc.DrawText(std::to_string((int)dir), 50,50);
-	//std::map<double, int>::iterator it = valueLocs.begin();
+	std::map<double, int>::iterator it = valueLocs.begin();
 
-	//if (dir == HORIZONTAL) {
-	//	dc.DrawLine(boundary[0], origin.y, boundary[1], origin.y); // Draw axis line
-	//	for (; it != valueLocs.end(); ++it) { // Draw axis ticks
-	//		dc.DrawLine(it->second, origin.y + 5, it->second, origin.y - 5);
-	//		dc.DrawText(std::to_string(it->first).substr(0,4), it->second, origin.y + 7);
-	//	}
-	//}
-	//else {
-	//	dc.DrawLine(origin.x, boundary[0], origin.x, boundary[1]);
-	//	for (; it != valueLocs.end(); ++it) {
-	//		dc.DrawLine(origin.x + 5, it->second, origin.x - 5, it->second);
-	//	}
-	//}
+	if (dir == HORIZONTAL) {
+		dc.DrawLine(boundary[0], origin.y, boundary[1], origin.y); // Draw axis line
+		for (; it != valueLocs.end(); ++it) { // Draw axis ticks
+			dc.DrawLine(it->second, origin.y + 5, it->second, origin.y - 5);
+			dc.DrawText(std::to_string(it->first).substr(0,4), it->second, origin.y + 7);
+		}
+	}
+	else {
+		dc.DrawLine(origin.x, boundary[0], origin.x, boundary[1]);
+		for (; it != valueLocs.end(); ++it) {
+			dc.DrawLine(origin.x + 5, it->second, origin.x - 5, it->second);
+		}
+	}
 	
 }
