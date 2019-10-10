f1cf0fdbb3501b56d3575ebc4e99bfd458fbcb98
Mark Bashqawi <mbashqawi@gmail.com>
2019-10-09 21:20:12 -0500

    Commented out basic draw functions due to being obsolete

--- src/AirfoilViewer.h
+++ src/AirfoilViewer.h
@@ -19,9 +19,9 @@ public:
 	void onPaintEvent(wxPaintEvent& event);
 private:
 	void onViewerBackButton(wxCommandEvent& event); // Defined in Main.cpp
-	void drawTicks(wxDC& dc, wxPoint& origin, wxPoint& beg, wxPoint& end, int dir, int n);
-	void drawTick(wxDC& dc, wxPoint pos, int dir);
-	void drawAxes(wxPaintDC& dc);
+	//void drawTicks(wxDC& dc, wxPoint& origin, wxPoint& beg, wxPoint& end, int dir, int n);
+	//void drawTick(wxDC& dc, wxPoint pos, int dir);
+	//void drawAxes(wxPaintDC& dc);
 };
 
 // Button IDs for event connection
