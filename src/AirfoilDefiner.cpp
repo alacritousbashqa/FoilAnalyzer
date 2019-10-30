82708b61fa622f7114ab32ca773027bdd66fc5c0
Mark Bashqawi <mbashqawi@gmail.com>
2019-10-30 13:08:36 -0500

    Added comments

--- src/AirfoilDefiner.cpp
+++ src/AirfoilDefiner.cpp
@@ -3,40 +3,46 @@
 AirfoilDefiner::AirfoilDefiner(const wxString& title)
 	: wxDialog(NULL, -1, title, wxDefaultPosition, wxSize(200, 230)) {
 
+	// Default values
 	type = -1;
 	nPanels = 50;
 
+	//Panel that holds the static boxes and text controls
 	wxPanel *panel = new wxPanel(this, -1);
 
 	wxBoxSizer *vbox = new wxBoxSizer(wxVERTICAL);
 	wxBoxSizer *hbox = new wxBoxSizer(wxHORIZONTAL);
 
+	// Static box containing the code and number of panels text controls
 	wxStaticBox *st = new wxStaticBox(panel, -1, wxT("NACA Type"),
 		wxPoint(5, 5), wxSize(170, textH * 9 + 20));
 	wxStaticText stext(panel, -1, wxT("Enter a NACA 4 or 5 digit code:"),
 		wxPoint(15, 2 * textH), wxSize(150, 32));
 	tc = new wxTextCtrl(panel, CODE_TEXT_CTRL_ID, wxT(""),
 		wxPoint(15, 4.5f * textH), wxSize(150,24));
-	tc->SetMaxLength(15);
+	tc->SetMaxLength(15); // Set code max length
 	wxStaticText stext1(panel, -1, wxT("Number of Panels:"),
 		wxPoint(15, 7 * textH), wxSize(150, 20));
 	tc1 = new wxTextCtrl(panel, -1, std::to_string(nPanels).c_str(),
 		wxPoint(15, 8.5f * textH), wxSize(50, 24));
-	tc1->SetMaxLength(4);
+	tc1->SetMaxLength(4); // Set number of panels max length
 
+	// Static box containing the unique identifier (the name) text control
 	wxStaticBox *st1 = new wxStaticBox(panel, -1, wxT("Name"),
 		wxPoint(5, 11 * textH), wxSize(170, textH * 7));
 	wxStaticText stext2(panel, -1, wxT("Enter a unique identifier for this airfoil:"),
 		wxPoint(15, 13 * textH), wxSize(150, 32));
 	nameTC = new wxTextCtrl(panel, -1, wxT(""),
 		wxPoint(15, 15.5f * textH), wxSize(150, 24));
-	nameTC->SetMaxLength(15);
-
+	nameTC->SetMaxLength(15); // Set max length of the name
+	
+	// Buttons
 	wxButton *okButton = new wxButton(this, OK_ID, wxT("Ok"),
 		wxDefaultPosition, wxSize(70, 30));
 	wxButton *closeButton = new wxButton(this, CANCEL_ID, wxT("Cancel"),
 		wxDefaultPosition, wxSize(70, 30));
 	
+	// Set final size of dialog
 	this->SetSize(200, 25 * textH);
 
 	hbox->Add(okButton, 1);
@@ -47,16 +53,18 @@ AirfoilDefiner::AirfoilDefiner(const wxString& title)
 
 	SetSizer(vbox);
 
+	// Connect the buttons to their functions
 	Connect(OK_ID, wxEVT_BUTTON, wxCommandEventHandler(AirfoilDefiner::onOK));
 	Connect(CANCEL_ID, wxEVT_BUTTON, wxCommandEventHandler(AirfoilDefiner::onCancel));
-
+	// Connect onTextChanged from the code text control to its function
 	Connect(CODE_TEXT_CTRL_ID, wxEVT_TEXT, wxCommandEventHandler(AirfoilDefiner::onCodeChange));
 
+	// Center and set focus to the code text control
 	Centre();
-	modalCode = ShowModal();
+	modalCode = ShowModal(); // Pause everything except this dialog
 	tc->SetFocus();
 
-	Destroy();
+	Destroy(); // Only gets called after EndModal is called
 }
 
 void AirfoilDefiner::onOK(wxCommandEvent& event) {
@@ -64,9 +72,11 @@ void AirfoilDefiner::onOK(wxCommandEvent& event) {
 	std::string tmp = tc1->GetLineText(0);
 	name = nameTC->GetLineText(0);
 
+	// If number of panels text is not empty and is numeric, set nPanels
 	if (tmp != "" && std::all_of(tmp.begin(), tmp.end(), ::isdigit)) {
 		nPanels = std::stoi(tmp);
 	}
+	// Else, set it to an invalid number
 	else {
 		nPanels = -1;
 	}
@@ -75,26 +85,31 @@ void AirfoilDefiner::onOK(wxCommandEvent& event) {
 		wxLogError("No name was entered! Please enter a name for the generated airfoil.");
 		return;
 	}
-
+	// If the code text is not empty and is numeric...
 	if (code != "" && std::all_of(code.begin(), code.end(), ::isdigit)) {
+		// If the number of panels is not in the valid range, return with an error 
 		if (nPanels < 10 || nPanels > 5000) {
 			nPanels = -1;
 			wxLogError("An invalid number of panels was entered! Number of panels must be between 10 and 5000!");
 			return;
 		}
+		// If the code is a 4 digit, close dialog
 		if (code.length() == 4) {
 			type = 4;
 			EndModal(modalCode);
 		}
+		// If the code is a 5 digit, close dialog
 		else if (code.length() == 5) {
 			type = 5;
 			EndModal(modalCode);
 		}
+		// Else, return an invalid type with error
 		else {
 			type = -1;
 			wxLogError("An invalid length was entered! Please use 4 or 5 digit series only!");
 		}
 	}
+	// Else, return an invalid type with error
 	else {
 		type = -1;
 		wxLogError("Invalid code! Code should be only numbers and be 4 or 5 digits long!");
@@ -102,13 +117,15 @@ void AirfoilDefiner::onOK(wxCommandEvent& event) {
 }
 
 void AirfoilDefiner::onCancel(wxCommandEvent& event) {
-	EndModal(modalCode);
+	EndModal(modalCode); // Close dialog
 }
 
 void AirfoilDefiner::onCodeChange(wxCommandEvent& event) {
+	// Clamp text length to max allowed characters
 	std::string c = tc->GetLineText(0);
 	if (c.length() > 10)
 		c = c.substr(0, 10);
+	// Set name text from code
 	nameTC->SetValue("NACA " + c);
 }
 
--- src/AirfoilDefiner.h
+++ src/AirfoilDefiner.h
@@ -14,7 +14,7 @@ enum {
 };
 
 class AirfoilDefiner : public wxDialog {
-	int textH = GetTextExtent("A").GetHeight();
+	int textH = GetTextExtent("A").GetHeight();	// Text height for positioning widgets instead of hard coded positions
 
 	std::string code;		// NACA code (and later other airfoils)
 	std::string name;		// Unique identifier for this airfoil
@@ -26,14 +26,35 @@ class AirfoilDefiner : public wxDialog {
 	wxTextCtrl* tc1;		// Number of panels text control
 	wxTextCtrl* nameTC;		// Name text control
 
+	/* Called when the code text is changed by either the user or by code. 
+	Sets the name based on the entered code in the form: NACA {code}.
+	*/
 	void onCodeChange(wxCommandEvent& event);
 
+	// Called when the OK button is pressed. Checks and returns the values from the text controls.
 	void onOK(wxCommandEvent& event);
+	// Called when the cancel button is pressed. Returns invalid values.
 	void onCancel(wxCommandEvent& event);
 public:
+	//-------------------------------------------------------------------------------------------------------------
+	// CONSTRUCTOR
+
 	AirfoilDefiner(const wxString& title);
+
+	//-------------------------------------------------------------------------------------------------------------
+	// GETTERS
+
+	// Returns the airfoil code entered in the top text box
 	std::string getCode();
+	// Returns the airfoil name entered in the bottom text box
 	std::string getName();
+	/* Returns the number of panels specified in the middle text box.
+	-1 is returned when the entered number is not valid
+	*/
 	int getNPanels();
+	/* Returns the NACA series based on the length of the code text (either 4 or 5 digit series).
+	-1 is returned when the type is not valid (neither 4 or 5)
+	*/
 	int getType();
+	//-------------------------------------------------------------------------------------------------------------
 };
