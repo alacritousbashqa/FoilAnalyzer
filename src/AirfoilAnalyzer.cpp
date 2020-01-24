7007f74dfe42f392d8bcc25745b530f9300b12ab
Mark Bashqawi <mbashqawi@gmail.com>
2020-01-23 21:22:58 -0600

    Added basic options panel layout

--- src/AirfoilAnalyzer.cpp
+++ src/AirfoilAnalyzer.cpp
@@ -1,3 +1,5 @@
+#include <wx/statline.h>
+
 #include "AirfoilAnalyzer.h"
 
 AnalyzerPanel::AnalyzerPanel(wxWindow* parent) 
@@ -11,11 +13,27 @@ AnalyzerPanel::AnalyzerPanel(wxWindow* parent)
 	aaGraphArea = drawAreaBoxSizer->Add(new wxPanel(this,7), wxEXPAND);
 	aaAirfoilArea = drawAreaBoxSizer->Add(new wxPanel(this,8), wxEXPAND);
 
-	// Sizer for the options menu
+	//-----------------------    OPTIONS PANEL    ------------------------------------------------------
 	optionsBoxSizer = new wxBoxSizer(wxVERTICAL);
 	wxPanel* optionsPanel = new wxPanel(this, -1, wxDefaultPosition, wxSize(300,500));
 	optionsPanel->SetSizer(optionsBoxSizer);
 	optionsPanel->SetBackgroundColour(wxColour(*wxLIGHT_GREY));
+
+	optionsBoxSizer->Add(new wxStaticText(optionsPanel, -1, "Analysis Options", wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE_HORIZONTAL), 0, wxALIGN_CENTER | wxTOP, 10);	// Box Title
+	optionsBoxSizer->Add(new wxStaticLine(optionsPanel, -1, wxDefaultPosition, wxSize(270, 2)), 0, wxALIGN_CENTER);																// Title Bar
+		// AIRFOIL SELECTION
+	wxStaticBoxSizer *airfoilBox = new wxStaticBoxSizer(wxVERTICAL, optionsPanel, "Airfoil");
+	optionsBoxSizer->Add(airfoilBox, 0, wxALIGN_CENTER | wxTOP, 15);
+	airfoilBox->Add(new wxStaticText(airfoilBox->GetStaticBox(), -1, "Choose thine foil:"));
+	airfoilBox->Add(new wxChoice(airfoilBox->GetStaticBox(), -1));
+		// ANALYSIS DEFINITION
+	wxStaticBoxSizer *analysisBox = new wxStaticBoxSizer(wxVERTICAL, optionsPanel, "Analysis Definition");
+	optionsBoxSizer->Add(analysisBox, 0, wxALIGN_CENTER | wxTOP, 15);
+	analysisBox->Add(new wxStaticText(analysisBox->GetStaticBox(), -1, "Setup analysis parameters here"));
+
+	optionsBoxSizer->AddStretchSpacer();																																		// Space between options and bottom button
+	optionsBoxSizer->Add(new wxButton(optionsPanel, -1, "Run Analysis"), 0, wxALIGN_CENTER | wxBOTTOM, 10);																		// Run button
+	//--------------------------------------------------------------------------------------------------
 
 	aaTopSizer->Add(drawAreaBoxSizer, 1, wxLEFT | wxRIGHT | wxBOTTOM | wxEXPAND, 10);
 	aaTopSizer->Add(optionsPanel, 0, wxLEFT | wxRIGHT | wxBOTTOM | wxEXPAND, 10);
